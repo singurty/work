f43d1e42aaa31f369f84431a63b6e87625083bba
singurty <singurty@gmail.com>
2021-06-24 19:25:13 +0545

    added interactive shell

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/singurty/fakework
 
 go 1.13
+
+require github.com/abiosoft/ishell/v2 v2.0.2
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"github.com/singurty/fakework/root"
-	"github.com/singurty/fakework/child"
 	"os"
+	"github.com/abiosoft/ishell/v2"
+	"github.com/singurty/fakework/child"
+	"github.com/singurty/fakework/root"
 )
 
 func main() {
 	if os.Args[1] == "child" {
 		child.Initialize("127.0.0.1", 8000)
 	} else if os.Args[1] == "root" {
-		root.Initialize("0.0.0.0", 8000)
+		rootShell()
+		//root.Initialize("0.0.0.0", 8000)
 	}
+}
+
+func rootShell() {
+	shell := ishell.New()
+	shell.AddCmd(&ishell.Cmd{
+		Name: "init",
+		Help: "Initialize root node",
+		Func: func(c *ishell.Context) {
+			c.Println("Initializing root node")
+			root.Initialize("0.0.0.0", 8000)
+		},
+	})
+	shell.Run()
 }
